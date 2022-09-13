ce727dee58b136f28517ff9d1a49828797d2c82e
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-09-13 12:51:05 +0300

    Add input control for editing

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,6 +6,8 @@ const TodoItem = ({ data, onDelete, onEdit, onComplete }) => {
     const [loading, setLoading] = useState(false);
     const [editingContent, setEditingContent] = useState('');
     const [inEditMode, setInEditMode] = useState(false);
+    const [error, setError] = useState("");
+    // const [isValid, setIsValid] = useState(false);
 
     const url = `https://6317245a82797be77ff48a60.mockapi.io/todos/${data.id}`;
 
@@ -35,17 +37,21 @@ const TodoItem = ({ data, onDelete, onEdit, onComplete }) => {
 
     const handleEditTodo = () => {
         if (inEditMode) {
-            axios.put(url,
-                {
-                    content: editingContent
-                }
-            )
-                .then((response) => {
-                    setInEditMode(false);
-                    onEdit(response.data)
+            editingContent.trim().length > 3 ?
+                axios.put(url,
+                    {
+                        content: editingContent
+                    }
+                )
+                    .then((response) => {
+                        setInEditMode(false);
+                        onEdit(response.data)
 
-                    editInput.current.className = "read-mode";
-                })
+                        editInput.current.className = "read-mode";
+                        setError("")
+
+                    }) : setError("The field must have more than three valid characters!")
+            // setIsValid(true)
         } else {
             setInEditMode(true);
             editInput.current.className = "edit-mode";
@@ -67,13 +73,25 @@ const TodoItem = ({ data, onDelete, onEdit, onComplete }) => {
 
     return (
         <>
-            <input type="text" readOnly={!inEditMode} className='read-mode' ref={editInput}
-                value={editingContent}
-                onChange={(e) => setEditingContent(e.target.value)}
-                onClick={() => handleComplete()}
-            />
-            <button onClick={() => handleEditTodo()} className={inEditMode ? 'save-btn' : 'edit-btn'}>{inEditMode ? "Save" : "Edit"}</button>
-            <button disabled={loading} className={loading ? 'loading-del-btn' : 'delete-btn'} onClick={() => deleteTodo(data.id)}>{loading ? 'Deleting...' : 'Delete'}</button>
+            <div className='todo-item-row'>
+                <div className='todo-item-input-error'>
+                    <input type="text" readOnly={!inEditMode}
+                        className='read-mode' ref={editInput}
+                        value={editingContent}
+                        onChange={(e) => setEditingContent(e.target.value)}
+                        onClick={() => handleComplete()} />
+                    <span className='edit-error'>{error}</span></div>
+                <button onClick={() => handleEditTodo()}
+                    className={inEditMode ? 'save-btn' : 'edit-btn'}
+                    disabled={loading}>
+                    {inEditMode ? "Save" : "Edit"}</button>
+                <button disabled={loading}
+                    className={loading ? 'loading-del-btn' : 'delete-btn'}
+                    onClick={() => deleteTodo(data.id)}>
+                    {loading ? 'Deleting...' : 'Delete'}
+                </button>
+            </div>
+
         </>);
 }
 
