559dd649c497b994139041f554e16b9f50570475
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-09-15 21:55:42 +0300

    Add theme context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useContext } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Todos from './components/Todos';
+import Login from './components/Login';
+import { ThemeProvider } from './contexts/ThemeContext';
+
 
 function App() {
 
   return (
-    <div className="App">
-      <Todos />
+    <div className='App'>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/todos" element={<Todos />} />
+          </Routes>
+        </BrowserRouter>
+
+      </ThemeProvider>
+
     </div>
   );
 }
