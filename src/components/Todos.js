1b748f365af4837193dfd6bcdf31a245cbae80c9
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-09-15 21:56:59 +0300

    Update Todos.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,16 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import { ThreeDots } from "react-loader-spinner";
 import TodoItem from './TodoItem';
+import ThemeContext, { ThemeProvider } from '../contexts/ThemeContext';
+import { useLocation } from 'react-router-dom';
 
 const Todos = () => {
 
     const [todos, setTodos] = useState([]);
     const [addTodo, setAddTodo] = useState('');
     const [error, setError] = useState('');
+    const [filtered, setFiltered] = useState([]);
+    const [currentFiltered, setCurrentFiltered] = useState("all");
+    const [loading, setLoading] = useState(false);
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    const location = useLocation();
 
     const fetchData = () => {
+        setLoading(true);
         axios.get("https://6317245a82797be77ff48a60.mockapi.io/todos")
-            .then(res => setTodos(res.data))
+            .then(res => {
+                setTodos(res.data);
+                setFiltered(res.data)
+                setLoading(false)
+            })
             .catch(error =>
                 console.log(error));
     }
@@ -24,6 +37,10 @@ const Todos = () => {
             curr.filter(item => {
                 return item.id !== id;
             }))
+        setFiltered(curr =>
+            curr.filter(item => {
+                return item.id !== id;
+            }))
     }
 
     const handleEdit = (editedTodo) => {
@@ -32,22 +49,24 @@ const Todos = () => {
         editedTodoItem = editedTodo;
 
         setTodos(editedTodoList);
+        setFiltered(editedTodoList)
     }
 
     const handleAddTodo = (e) => {
         e.preventDefault()
 
-        if (addTodo.trim().length > 3) {
+        if (addTodo.trim().length > 2) {
             axios.post(`https://6317245a82797be77ff48a60.mockapi.io/todos/`, {
                 content: addTodo
             }).then((response) => {
                 setTodos([...todos, response.data])
+                setFiltered([...todos, response.data])
                 setAddTodo('');
             })
             setError("")
         }
         else {
-            setError("The input field must have more than three valid characters!");
+            setError("The input field must have three valid characters at least!");
             setAddTodo('');
         }
     }
@@ -58,24 +77,80 @@ const Todos = () => {
         console.log("second", editedTodoList);
 
         setTodos(editedTodoList);
+        // let filteredTodoItem = filtered.find((item) => item.id === id)
+        // filteredTodoItem.isCompleted 
+        // setFiltered(
+        //     filtered.map(todo => {
+        //         if (todo.id === id) {
+        //             return {
+        //                 ...todo,
+        //                 isCompleted: !todo.isCompleted
+        //             }
+        //         }
+        //         return todo;
+        //     })
+        // );
     }
 
-    return <div className='container'>
-        <form>
-            <h1 className='title'>TODOS</h1>
-            <input className='add-input' value={addTodo} onChange={(e) => setAddTodo(e.target.value)} />
-            <button className='add-btn' onClick={handleAddTodo}>Add</button>
-            <p>{error}</p>
-        </form>
-        <ul > {
-            todos.map((todo) =>
-                <li key={todo.id}>
-                    <TodoItem data={todo} onDelete={handleDelete} onEdit={handleEdit} onComplete={onComplete} />
-                </li>
-            )
+    //     useEffect(() => {
+    //   setFiltered(fetchData())
+    //     }, [third])
+
+    // const filteredTodos = todos.filter(item => item.isCompleted)
+    // console.log('COMP', filteredTodos);
+    const handleListingTodos = (filter) => {
+        setCurrentFiltered(filter)
+        if (filter === 'all') {
+            setFiltered([...todos])
+        } else if (filter === 'active') {
+            setFiltered(todos.filter(todo => !todo.isCompleted))
         }
-        </ul>
+        else {
+            setFiltered(todos.filter(todo => todo.isCompleted))
+        }
+        // const filteredTodos = todos.filter(item => item.isCompleted)
+        // let listedTodo = filteredTodos.includes(isCompleted)
+        // listedTodo ? 'completed' : 'all'
+    }
+
+    // <ThemeProvider>
+    return <div className='container'>
+        <div className='header'>
+            <p className='welcome'>Welcome {location.state.username}!</p>
+            <button onClick={toggleTheme} className="theme-btn">
+                {theme === 'dark' ? <span>🌞</span> : <span>🌙</span>}
+            </button></div>
+        <div className='column' id='column'>
+            <form className='form-container'>
+                <h1 className='title'>TODOS</h1>
+                <input className='add-input' value={addTodo} onChange={(e) => setAddTodo(e.target.value)} />
+                <button className='add-btn' onClick={handleAddTodo}>Add</button> </form>
+            <span className='add-error'>{error}</span>
+            {loading ? <ThreeDots
+                height="80"
+                width="80"
+                radius="9"
+                color="#FFF"
+                ariaLabel="three-dots-loading"
+                wrapperStyle={{}}
+                wrapperClassName=""
+                visible={true}
+            /> : <ul>
+                {
+                    todos.map((todo) =>
+                        <li key={todo.id}>
+                            <TodoItem data={todo} onDelete={handleDelete} onEdit={handleEdit} onComplete={onComplete} />
+                        </li>
+                    )
+                }
+            </ul>}
+            <div className='listing-btn-container'>
+                <button onClick={() => handleListingTodos('all')} value='all' className='listing-btn'>All</button>
+                <button onClick={() => handleListingTodos('active')} className='listing-btn' value='active'>Active</button>
+                <button onClick={() => handleListingTodos('completed')} className='listing-btn' value='completed'>Completed</button></div>
+        </div>
     </div>
+    // </ThemeProvider>
 }
 
 export default Todos;
